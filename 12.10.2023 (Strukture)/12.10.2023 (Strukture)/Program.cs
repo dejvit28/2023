7f971d56251345def5cde5a58af4e6509f4bef4d
dejvit28 <122635996+dejvit28@users.noreply.github.com>
2023-10-12 10:55:12 +0200

    Add files via upload

---
+++ 12.10.2023 (Strukture)/12.10.2023 (Strukture)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._10._2023__Strukture_
+{
+    internal class Program
+    {
+        struct Vektor
+        {
+            public double x, y, z;
+            public override string ToString()
+            {
+                return "("+x+", "+y+", "+z+")";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Vektor v;
+            v.x =  1;
+            v.y = -1;
+            v.z =  1;
+            Console.WriteLine(v.ToString());
+
+            Vektor z=new Vektor();
+            z.x =  2; 
+            z.y = -1; 
+            z.z =  1;
+            Console.WriteLine(z.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
